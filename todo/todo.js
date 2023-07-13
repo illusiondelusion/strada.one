365f2ae0842d6f0300f8aaabc6a6bcf50a199533
illusiondelusion <kseniaschrodinger@gmail.com>
2023-07-13 12:59:34 +0300

    feat: add showlist function

--- todo/todo.js
+++ todo/todo.js
@@ -1,29 +1,88 @@
-const todoList = {
+const STATUS = {
+    'TODO': 'To Do',
+    'IN_PROGRESS': 'In Progress',
+    'DONE': 'Done'
+};
 
-    list: {
-	"create a new practice task": "In Progress",
-	"make a bed": "Done",
-	"write a post": "To Do",
-    },
+const list = {
+	'create a new practice task': STATUS.IN_PROGRESS,
+	'go to work': STATUS.DONE,
+	'write a post': STATUS.TODO,
+};
 
-    addTask(task) {
-        this.list[task] = 'To Do';
-    },
+const errorMessage = {
+    'WRONG_STATUS': 'No such status!',
+    'WRONG_TASK': 'No such task!',
+};
 
-    changeStatus(task,status) {
-        this.list[task] = status;
-    },
-
-    deleteTask(task) {
-        delete this.list[task];
-    },
-
-    showList() {
-        console.log(this.list);
-    },
-
+function addTask(task) {
+    list[task] = STATUS.toDo;
 }
 
-todoList.addTask('do this');
-todoList.changeStatus('write a post','In Progress')
-todoList.showList();
+function changeStatus(task, status) {
+    if (!(task in list)) {
+        console.log(errorMessage.WRONG_TASK);
+    } else if (status !== STATUS.TODO && status !== STATUS.IN_PROGRESS && status !== STATUS.DONE) {
+        console.log(errorMessage.WRONG_STATUS);
+    } else {
+        list[task] = status;
+    }
+}
+
+function deleteTask(task) {
+    if (task in list) {
+        delete list[task];
+    } else {
+        console.log(errorMessage.WRONG_TASK);
+    }
+}
+
+function showList() {
+    console.group(`${STATUS.TODO}:`)
+    let check = false;
+    for (key in list) {
+        if (list[key] === STATUS.TODO) {
+            check = true;
+            console.log(key);
+        }
+    }
+
+    if (check === false) {
+        console.log('-');
+    }
+    console.groupEnd();
+
+    console.group(`${STATUS.IN_PROGRESS}:`)
+    check = false;
+    for (key in list) {
+        if (list[key] === STATUS.IN_PROGRESS) {
+            check = true;
+            console.log(key);
+        }
+    }
+
+    if (check === false) {
+        console.log('-');
+    }
+    console.groupEnd();
+
+    console.group(`${STATUS.DONE}:`)
+    check = false;
+    for (key in list) {
+        if (list[key] === STATUS.DONE) {
+            check = true;
+            console.log(key);
+        }
+    }
+
+    if (check === false) {
+        console.log('-');
+    }
+    console.groupEnd();
+}
+
+addTask('do this');
+changeStatus('write a post','In Progress');
+changeStatus('go to work','eee');
+deleteTask('pipich');
+showList();
