c17691dbe982b0c18e8a8f7a22fdc0386aed4986
illusiondelusion <kseniaschrodinger@gmail.com>
2023-07-13 14:38:12 +0300

    feat: done string_practice task

--- string_practice.js
+++ string_practice.js
@@ -1,3 +1,13 @@
 function showVerticalMessage(str) {
-
+    if (typeof str !== 'string') {
+        console.log('Please enter a string!');
+    } else {
+        const newStr = str.split('').join('\n');
+        let result = newStr[0].toUpperCase() + newStr.slice(1, 13);
+        console.log(result);
+    }
 }
+
+showVerticalMessage('strada');
+showVerticalMessage('great frontend marathon');
+showVerticalMessage(1234566);
